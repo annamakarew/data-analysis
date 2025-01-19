b324879e3479df73fc87902856afeb2ac718cf16
annamakarew <makarewiczanna@yahoo.com>
2025-01-19 12:06:45 -0800

    Finished polars analysis and results

---
+++ Week2/polars_analysis.py
@@ -0,0 +1,45 @@
+import polars as pl
+import sys
+import time
+
+# Check for valid args
+if len(sys.argv) < 5:
+    print("Intended use: python3 week1.py YYYY-MM-DD HH YYYY-MM-DD HH")
+    sys.exit()
+
+start_date = sys.argv[1]
+start_hour = sys.argv[2]
+end_date = sys.argv[3]
+end_hour = sys.argv[4]
+
+# Combine the date and hour into strings
+start_str = start_date + " " + start_hour + ":00:00"  # Format it as YYYY-MM-DD HH:00:00
+end_str = end_date + " " + end_hour + ":00:00"  # Format it as YYYY-MM-DD HH:00:00
+
+# Validate that end date is not before start date
+if start_str > end_str:
+    print("Error: start date is after end date")
+    sys.exit()
+
+# Start the timer
+start_time = time.perf_counter_ns()
+
+df = pl.read_csv('../../../../../Downloads/2022_place_canvas_history.csv')
+# Filter the data based on the timestamp range
+most_common_color = df.filter(
+    (pl.col("timestamp") >= pl.lit(start_str)) & (pl.col("timestamp") <= pl.lit(end_str))
+)['pixel_color'].mode()
+most_common_coord = df.filter(
+    (pl.col("timestamp") >= pl.lit(start_str)) & (pl.col("timestamp") <= pl.lit(end_str))
+)['coordinate'].mode()
+
+# Stop the timer
+end_time = time.perf_counter_ns()
+
+# Calculate execution time in milliseconds
+execution_time = (end_time - start_time) / 1_000_000
+
+# Output the results
+print(f"Execution time: {execution_time:.3f} ms")
+print(f"Most common color: {most_common_color}")
+print(f"Most common coord: {most_common_coord}")
---
+++ Week2/polars_results.md
@@ -0,0 +1,19 @@
+# Pandas Results
+
+## 1-Hour Timeframe
+- **Timeframe:** 2022-04-01 14 to 2022-04-01 15
+- **Execution Time:** 343382.942 ms
+- **Most Placed Color:** #000000
+- **Most Placed Pixel Location:** (859, 766)
+
+## 3-Hour Timeframe
+- **Timeframe:** 2022-04-01 14 to 2022-04-01 17
+- **Execution Time:** 313220.921 ms
+- **Most Placed Color:** #000000
+- **Most Placed Pixel Location:** (859, 766)
+
+## 6-Hour Timeframe
+- **Timeframe:** 2022-04-01 12 to 2022-04-01 20
+- **Execution Time:** 309073.431 ms
+- **Most Placed Color:** #000000
+- **Most Placed Pixel Location:** (859, 766)
